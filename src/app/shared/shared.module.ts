fbfa374bb63b71cab773c672ddb1db9a815bf3ff
cyberkrane <cyberkrane@gmail.com>
2023-12-26 20:22:07 -0300

    eliminacion del navbar en el modulo shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar/navbar.component';
-import { FooterComponent } from './navbar/footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent,
-    NavbarComponent,
     FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent,
     FooterComponent
   ]
 })
